1da7e98c26bbbfc58c7dc479bc77ab205afeb5a4
Phillip Malboeuf <phil@boeuf.coffee>
2018-07-11 13:32:31 -0400

    mobile layouts

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -23,13 +23,16 @@ interface State {}
 
 
 @context
-export class Index extends React.Component<Props, State> {
+export class Index extends React.PureComponent<Props, State> {
 
-  private photos: HTMLDivElement[]
+  private photos: HTMLDivElement[] = []
+  private vertical: boolean
 
   constructor(props: Props) {
     super(props)
     this.state = {}
+
+    this.vertical = window.innerWidth < window.innerHeight
   }
 
   componentDidMount() {
@@ -38,27 +41,24 @@ export class Index extends React.Component<Props, State> {
   }
 
   componentWillUnmount(){
+    delete this.photos
+    this.photos = []
     window.removeEventListener('scroll', this.scroll.bind(this))
   }
 
   private scroll() {
-    this.photos.forEach(photo => photo.style.transform = `translateY(-${window.scrollY/3}px)`)
+    this.photos.forEach(photo => photo.style.transform = `translateY(-${window.scrollY/( this.vertical ? 10 : 2.5 )}px)`)
   }
 
   public render() {
     return <div className='boxes'>
       <div className='box box--full'>
-        <div className='box__top_right' style={{zIndex: 4}}>
-          {/* <a href='/' className='button--transparent paragraph_medium' onClick={()=> document.cookie = `locale=${this.props.context.pieces.locale === 'en_CA' ? 'fr_CA' : 'en_CA'}`}>
-            <P r='index' k='other_language' />
-          </a> */}
-        </div>
-        <div className='grid box__top_left' style={{pointerEvents: 'auto'}} ref={element => this.photos = Array.from(element.querySelectorAll('.col'))}>
-          <div className='col col--6of12' />
-          <div className='col col--3of12'><Img r='index' k='photo_1' /></div>
-          <div className='col col--3of12' />
-          <div className='col col--9of12' />
-          <div className='col col--3of12 map' style={{zIndex: 3}}>
+        <div className='grid box__photos'>
+          <div className='col col--6of12 col--tablet_portrait--4of12 col--tablet_portrait--first' />
+          <div className='col col--3of12 col--tablet_portrait--4of12 col--tablet_portrait--first' ref={element => this.photos.push(element)}><Img r='index' k='photo_1' /></div>
+          <div className='col col--3of12 col--tablet_portrait--4of12 col--tablet_portrait--first' />
+          <div className='col col--9of12 col--tablet_portrait--8of12 col--tablet_portrait--first' />
+          <div className='col col--3of12 col--tablet_portrait--4of12 col--tablet_portrait--first map' style={{zIndex: 2}} ref={element => this.photos.push(element)}>
             <GoogleMap
               zoom={13}
               center={{
@@ -75,31 +75,39 @@ export class Index extends React.Component<Props, State> {
                 home: true
               }]} />
           </div>
-          <div className='col col--6of12' style={{zIndex: 3}}><Img r='index' k='photo_3' /></div>
-          <div className='col col--6of12' />
-          <div className='col col--9of12' />
-          <div className='col col--3of12'><Img r='index' k='photo_4' /></div>
-          <div className='col col--3of12'><Img r='index' k='photo_5' /></div>
-          <div className='col col--9of12' />
-          <div className='col col--6of12' />
-          <div className='col col--6of12' style={{zIndex: 3}}><Img r='index' k='photo_6' /></div>
-          <div className='col col--3of12' style={{zIndex: 2}}><Img r='index' k='photo_7' /></div>
-          <div className='col col--9of12' />
-          <div className='col col--9of12' />
-          <div className='col col--3of12'><Img r='index' k='photo_8' /></div>
-          <div className='col col--3of12' />
-          <div className='col col--3of12' style={{zIndex: 2}}><Img r='index' k='photo_9' /></div>
-          <div className='col col--6of12' />
-          <div className='col col--12of12' style={{height: '25vw'}} />
-          <div className='col col--6of12' />
-          <div className='col col--3of12' style={{zIndex: 3}}><Img r='index' k='photo_10' /></div>
+          <div className='col col--tablet_portrait--12of12 tablet_portrait_only' style={{height: '33vw'}} />
+          <div className='col col--6of12 col--tablet_portrait--8of12' style={{zIndex: 3}} ref={element => this.photos.push(element)}><Img r='index' k='photo_3' /></div>
+          <div className='col col--6of12 col--tablet_portrait--4of12' />
+          <div className='col col--tablet_portrait--12of12 tablet_portrait_only' style={{height: '33vw'}} />
+          <div className='col col--9of12 col--tablet_portrait--4of12' />
+          <div className='col col--3of12 col--tablet_portrait--4of12' ref={element => this.photos.push(element)}><Img r='index' k='photo_4' /></div>
+          <div className='col col--3of12 col--tablet_portrait--4of12 col--tablet_portrait--first' ref={element => this.photos.push(element)}><Img r='index' k='photo_5' /></div>
+          <div className='col col--9of12 hide_on_tablet_portrait' />
+          <div className='col col--6of12 hide_on_tablet_portrait' />
+          <div className='col col--tablet_portrait--12of12 tablet_portrait_only' style={{height: '33vw'}} />
+          <div className='col col--6of12 col--tablet_portrait--8of12' style={{zIndex: 3}} ref={element => this.photos.push(element)}><Img r='index' k='photo_6' /></div>
+          <div className='col col--tablet_portrait--12of12 tablet_portrait_only' style={{height: '33vw'}} />
+          <div className='col col--3of12 col--tablet_portrait--4of12' style={{zIndex: 2}} ref={element => this.photos.push(element)}><Img r='index' k='photo_7' /></div>
+          <div className='col col--9of12 hide_on_tablet_portrait' />
+          <div className='col col--tablet_portrait--12of12 tablet_portrait_only' style={{height: '33vw'}} />
+          <div className='col col--9of12 col--tablet_portrait--8of12' />
+          <div className='col col--3of12 col--tablet_portrait--4of12' ref={element => this.photos.push(element)}><Img r='index' k='photo_8' /></div>
+          <div className='col col--3of12 hide_on_tablet_portrait' />
+          <div className='col col--tablet_portrait--12of12 tablet_portrait_only' style={{height: '33vw'}} />
+          <div className='col col--3of12 col--tablet_portrait--4of12' style={{zIndex: 2}} ref={element => this.photos.push(element)}><Img r='index' k='photo_9' /></div>
+          <div className='col col--6of12 hide_on_tablet_portrait' />
+          <div className='col col--12of12 hide_on_tablet_portrait' style={{height: '25vw'}} />
+          <div className='col col--tablet_portrait--12of12 tablet_portrait_only' style={{height: '33vw'}} />
+          <div className='col col--tablet_portrait--12of12 tablet_portrait_only' style={{height: '33vw'}} />
+          <div className='col col--6of12 col--tablet_portrait--4of12' />
+          <div className='col col--3of12 col--tablet_portrait--4of12' style={{zIndex: 3}} ref={element => this.photos.push(element)}><Img r='index' k='photo_10' /></div>
         </div>
 
         <div className='box__border' />
 
         <div className='grid grid--center' style={{zIndex: 2}}>
           <a id='benvenuto' className='a--anchor' />
-          <div className='col col--6of12'>
+          <div className='col col--6of12 col--tablet_portrait--8of12 col--phone--10of12'>
             <div className='grid grid--guttered'>
               <div className='col col--12of12'>
                 <h1 className='header_giant'><P r='index' k='title' /></h1>
@@ -107,7 +115,7 @@ export class Index extends React.Component<Props, State> {
 
               <div className='col col--12of12'>
                 <A r='menu' k='address_link' blank className='info_medium'> 
-                  <small className='info_small'><P r='index' k='address_more' /></small><br />
+                  <small className='info_small inline_block'><P r='index' k='address_more' /></small><br />
                   <P r='index' k='address' />
                 </A>
               </div>
@@ -147,15 +155,15 @@ export class Index extends React.Component<Props, State> {
 
               <div className='col col--12of12'>
                 <a id='pizza' className='a--anchor' />
-                <p className='header_big'><P r='index' k='pizza_1' /></p>
+                <p className='header_big text_right_on_tablet_portrait'><P r='index' k='pizza_1' /></p>
               </div>
               <div className='col col--4of12 col--rotate'>
-                <p className='header_small' style={{maxHeight: '40vh'}}><P r='index' k='pizza_2' /></p>
+                <p className='header_small' style={{maxHeight: '45vh'}}><P r='index' k='pizza_2' /></p>
               </div>
 
               <div className='col col--8of12'>
-                <p className='info_small big_bottom'><P r='index' k='pizza_3' /></p>
-                <p className='paragraph_medium'><P r='index' k='pizza_4' /></p>
+                <p className='info_small big_bottom text_right_on_tablet_portrait'><P r='index' k='pizza_3' /></p>
+                <p className='paragraph_medium text_right_on_tablet_portrait'><P r='index' k='pizza_4' /></p>
               </div>
 
               <div className='col col--spacer' />
@@ -166,10 +174,8 @@ export class Index extends React.Component<Props, State> {
                 <p className='header_small'><P r='index' k='seasons_3' /></p>
               </div>
               <div className='col col--4of12 col--rotate col--rotate--inverse'>
-                <p className='header_medium' style={{maxHeight: '40vh'}}><P r='index' k='seasons_2' /></p>
+                <p className='header_medium' style={{maxHeight: '45vh'}}><P r='index' k='seasons_2' /></p>
               </div>
-
-              <div className='col col--spacer' />
             </div>
           </div>
         </div>

596dbf6b7fb3085e14f1512f7da2ee754a6d4eba
Phillip Malboeuf <phil@boeuf.coffee>
2018-07-11 13:32:15 -0400

    menu toggle

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -17,7 +17,9 @@ interface Props {
     }
   }
 }
-interface State {}
+interface State {
+  visible: boolean
+}
 
 
 @context
@@ -25,7 +27,9 @@ export class Menu extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props)
-    this.state = {}
+    this.state = {
+      visible: false
+    }
   }
 
   componentDidMount() {
@@ -36,12 +40,32 @@ export class Menu extends React.Component<Props, State> {
 
   }
 
+  public toggle(e: React.MouseEvent<HTMLButtonElement>) {
+    e.currentTarget.blur()
+    this.setState({
+      visible: !this.state.visible
+    })
+  }
+
+  private linkclick(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault()
+    this.setState({
+      visible: false
+    })
+
+    window.scroll({
+      left: 0,
+      top: document.querySelector(e.currentTarget.getAttribute('href')).offsetTop,
+      behavior: 'smooth'
+    })
+  }
+
   public render() {
-    return <nav className='menu grid grid--vertically_spaced'>
-      <a href='#contact' className='header_big'><P r='menu' k='contact' /></a>
-      <a href='#benvenuto' className='header_big'><P r='menu' k='benvenuto' /></a>
-      <a href='#pizza' className='header_big'><P r='menu' k='pizza' /></a>
-      <a href='#local' className='header_big'><P r='menu' k='local' /></a>
+    const nav = <>
+      <a href='#contact' onClick={(e)=> this.linkclick(e)} className='header_big'><P r='menu' k='contact' /></a>
+      <a href='#benvenuto' onClick={(e)=> this.linkclick(e)} className='header_big'><P r='menu' k='benvenuto' /></a>
+      <a href='#pizza' onClick={(e)=> this.linkclick(e)} className='header_big'><P r='menu' k='pizza' /></a>
+      <a href='#local' onClick={(e)=> this.linkclick(e)} className='header_big'><P r='menu' k='local' /></a>
       <a href='/' className='paragraph_big' onClick={()=> document.cookie = `locale=${this.props.context.pieces.locale === 'en_CA' ? 'fr_CA' : 'en_CA'}`}>
         <P r='menu' k='other_language' />
       </a>
@@ -53,6 +77,19 @@ export class Menu extends React.Component<Props, State> {
         <A r='menu' k='instagram_link' className='header_small' blank><P r='menu' k='instagram' /></A>
         <A r='menu' k='email_link' className='header_small' blank><P r='menu' k='email' /></A>
       </div>
-    </nav>
+    </>
+
+    return <>
+      <button className={`button--transparent menu_button${this.state.visible ? ' menu_button--toggled' : ''}`}
+        onClick={(e)=> this.toggle(e)}>
+        <Icon i='menu' />
+      </button>
+      <nav className={`menu`}>
+        {nav}
+      </nav>
+      <nav className={`menu menu--fixed${this.state.visible ? ' menu--visible' : ''}`}>
+        {nav}
+      </nav>
+    </>
   }
 }
